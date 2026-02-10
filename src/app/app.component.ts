0254935dc11881c0431a97618af423e6891d1dd8
ihassis <ihassis@insea.ac.ma>
2026-02-10 10:46:16 +0000

    les composantes du formulaire sont ajoutés avec succes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CandidateFormComponent } from "./candidate-form/candidate-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CandidateFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/candidate-form/candidate-form.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-candidate-form',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './candidate-form.component.html',
+  styleUrl: './candidate-form.component.css'
+})
+export class CandidateFormComponent {
+  
+  user = {
+    completename:'',
+    email: '',
+    niveau: ''
+  };
+  level:String = 'junior';
+  accepted:boolean = false;
+  options = [
+    {id:1, label:'CDI', checked:false},
+    {id:2,label:'freelance',checked:false}
+  ]
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Formulaire soumis ! Données :', this.user);
+      alert('Données soumises avec succès ! (Voir console)');
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
+
+}
