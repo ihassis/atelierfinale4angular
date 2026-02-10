fc47b41264756686c0ff720c1f69fbd62aa9474d
ihassis <ihassis@insea.ac.ma>
2026-02-10 12:07:38 +0000

    mon formulaire marche

--- src/app/candidate-form/candidate-form.component.ts
+++ src/app/candidate-form/candidate-form.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { JsonPipe} from '@angular/common';
 @Component({
   selector: 'app-candidate-form',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './candidate-form.component.html',
   styleUrl: './candidate-form.component.css'
 })
 export class CandidateFormComponent {
-  
   user = {
     completename:'',
     email: '',
     niveau: ''
   };
-  level:String = 'junior';
-  accepted:boolean = false;
-  immediat:boolean = false;
+  selectedNiveau!: string;
+  Niveau!:String;
+  get level(): string {
+    return this.options
+      .filter(opt => opt.checked)
+      .map(opt => opt.label)
+      .join(', ');
+  }
+  // level!:String;
+  accepted!:boolean;
+  immediat!:boolean;
+  pitchPersonnel:String = '';
   options = [
     {id:1, label:'CDI', checked:false},
     {id:2,label:'freelance',checked:false}
@@ -25,6 +34,8 @@ export class CandidateFormComponent {
 
   onSubmit(form: NgForm) {
     if (form.valid) {
+      console.log('Niveau sélectionné:', this.selectedNiveau);
+      console.log('Contrats sélectionnés:', this.level);
       console.log('Formulaire soumis ! Données :', this.user);
       alert('Données soumises avec succès ! (Voir console)');
     } else {
