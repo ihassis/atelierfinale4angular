e380b59caccdbb26ca295ae849b27fac3bf2069c
Ikram2025-2025 <ikramaoutghite74@gmail.com>
2026-02-10 10:56:12 +0100

    Ajout du formulaire Candidate Smart Form

---
+++ src/app/candidate-form/candidate-form.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './candidate-form.component.html',
+  styleUrl: './candidate-form.component.css'
+})
+export class CandidateFormComponent {
+    candidate = {
+    preferences: {
+      contract: 'CDI',   
+      available: false  
+    }
+  };
+
+}
+
+
